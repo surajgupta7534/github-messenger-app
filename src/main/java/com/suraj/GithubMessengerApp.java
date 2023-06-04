6018abeb3354da33d3d87f36015c0305c962269d
Suraj Gupta <surajgupta7534@gmail.com>
2023-06-04 15:01:29 +0530

    Added datastax properties to connect application cassandra

--- src/main/java/com/suraj/GithubMessengerApp.java
+++ src/main/java/com/suraj/GithubMessengerApp.java
@@ -1,10 +1,11 @@
 package com.suraj;
 
+import java.nio.file.Path;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -14,12 +15,12 @@ public class GithubMessengerApp {
 	public static void main(String[] args) {
 		SpringApplication.run(GithubMessengerApp.class, args);
 	}
-
-	@RequestMapping("/user")
-	public String user(@AuthenticationPrincipal OAuth2User principal) {
-		System.out.println(principal);	
-		return principal.getAttribute("login");
-	}
 	
-
+	@Bean
+	public CqlSessionBuilderCustomizer sessionBuildedrCustomizer(DataStaxAstraProperties astraProperties) {
+		
+		Path bundle = astraProperties.getSecureConnectBundle().toPath();
+		return builder -> builder.withCloudSecureConnectBundle(bundle);
+		
+	}
 } 
